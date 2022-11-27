7bd33e7b48c8152a08ad97e629abb0ebc0d48ecf
walid <walid.abdullah.7e5@itb.cat>
2022-11-27 16:17:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/animelist/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.example.animelist
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        val intent = Intent(this, MainActivity::class.java)
+        Handler().postDelayed({
+            startActivity(intent)
+            finish()
+        }, 5000)
+    }
+
+}
