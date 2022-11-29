fd741f9dd3d77e8a2c7e40474f76b34bcf69559f
walid <walid@waled.com>
2022-11-29 19:27:27 +0100

    Header del layout del fragment_anime_list.xml

---
+++ app/src/main/java/com/example/animelist/AnimeList.kt
@@ -0,0 +1,30 @@
+package com.example.animelist
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AnimeList : Fragment() {
+
+    companion object {
+        fun newInstance() = AnimeList()
+    }
+
+    private lateinit var viewModel: AnimeListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_anime_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[AnimeListViewModel::class.java]
+        // TODO: Use the ViewModel
+    }
+}
---
+++ app/src/main/java/com/example/animelist/AnimeListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.animelist
+
+import androidx.lifecycle.ViewModel
+
+class AnimeListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
